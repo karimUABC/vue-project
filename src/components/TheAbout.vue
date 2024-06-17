c771b892d61f70323da0702847b30d191ff49279
softwareEservices <software@eservicesmx.com>
2024-06-17 15:17:54 -0700

    cambios de prueba

---
+++ src/components/TheAbout.vue
@@ -0,0 +1,30 @@
+<script setup>
+import AboutItem from './AboutItem.vue'
+import DocumentationIcon from './icons/IconDocumentation.vue'
+
+</script>
+
+<template>
+
+  <AboutItem>
+    <template #icon>
+      <DocumentationIcon />
+    </template>
+
+
+    <template #name1>
+     ik
+    </template>
+
+    <template #te>
+     ik35423543
+    </template>
+  </AboutItem>
+
+
+  
+
+
+
+
+</template>
--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -8,12 +8,13 @@ import SupportIcon from './icons/IconSupport.vue'
 </script>
 
 <template>
+
   <WelcomeItem>
     <template #icon>
       <DocumentationIcon />
     </template>
     <template #heading>Documentation</template>
-
+  
     Vue’s
     <a href="https://vuejs.org/" target="_blank" rel="noopener">official documentation</a>
     provides you with all information you need to get started.
